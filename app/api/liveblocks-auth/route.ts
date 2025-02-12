1107bc94488fd61b0483885ee9458743968d56db
BDunnCode <135765583+BDunnCode@users.noreply.github.com>
2025-02-12 02:33:01 -0600

    add api route

---
+++ app/api/liveblocks-auth/route.ts
@@ -0,0 +1,38 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { Liveblocks } from "@liveblocks/node";
+
+const liveblocks = new Liveblocks({
+  secret: "sk_dev_kMmE4ejCOgjzDS7E4-gPbcXs5JDPa-yR0LWJYyKUTN7zPk0PSs2xd1Y2DARPJWZF",
+});
+
+export async function POST(request: Request) {
+  const clerkUser = await currentUser();
+
+  if(!clerkUser) redirect('/sign-in');
+
+  const { id, firstName, lastName, emailAddresses, imageUrl } = 
+  clerkUser;
+
+  // Get the current user from your database
+  const user = {
+    id: clerkUser?.id,
+    info: {
+      id,
+      name: `${firstName} ${lastName}`,
+      email: emailAddresses[0].emailAddress,
+      avatar: imageUrl,
+      color: ''
+    }
+  }
+
+  // Identify the user and return the result
+  const { status, body } = await liveblocks.identifyUser(
+    {
+      userId: user.id,
+      groupIds, // Optional
+    },
+    { userInfo: user.metadata },
+  );
+
+  return new Response(body, { status });
+}
