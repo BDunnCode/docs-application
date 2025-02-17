828abb91c84aca9988fcc467a0d344e42173c9d8
BDunnCode <135765583+BDunnCode@users.noreply.github.com>
2025-02-17 03:50:21 -0600

    next commit

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -17,6 +17,7 @@ import { FloatingComposer, FloatingThreads, liveblocksConfig, LiveblocksPlugin, 
 import Loader from '../Loader.tsx';
 import { useThreads } from '@liveblocks/react/suspense';
 import Comments from '../Comments.tsx';
+import { DeleteModal } from '../DeleteModal.tsx';
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -46,7 +47,7 @@ export function Editor({ roomId, currentUserType }: { roomId: string, currentUse
       <div className="editor-container size-full">
         <div className="toolbar-wrapper flex min-w-full justify-between">
           <ToolbarPlugin />
-          {/* {currentUserType === 'editor' && <DeleteModal roomId={roomId} /> */}
+          {currentUserType === 'editor' && <DeleteModal roomId={roomId} />}
         </div>
 
         <div className="editor-wrapper flex flex-col items-center justify-start">
